87d2bba71d0afa8623b8c19ba21e65a111fcc78b
akashpamal <akashpamal@gmail.com>
2023-03-25 16:03:27 -0400

    deleted extra EchoVision projects

--- EchoVision3/App.js
+++ EchoVision3/App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import { Audio } from 'expo-av';
 
 export default function App() {
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(require('./assets/test1.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+        console.log('Unloading Sound');
+        sound.unloadAsync();
+      }
+      : undefined;
+  }, [sound]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text lala/>
+      <Text lala/>
+      <Text lala/>
+      <Text lala/>
+      <Text lala/>
+      <Button title="Play Sound" onPress={playSound} />
     </View>
   );
+  // return  <Button title="Play Sound" onPress={playSound} />
+
+  // return playSound;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({ container: { justifyContent: 'center' } });
